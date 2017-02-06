b6ca993a313173d83aaaf211ed552b949e642a82
Alex Forbes-Reed <github@alx.red>
2017-02-06 11:41:31 +0000

    refactored http helper, made it cleaner

--- helpers/http.go
+++ helpers/http.go
@@ -22,26 +22,34 @@ type HTTPHelper struct {
 	Client  *http.Client
 }
 
-// PostForm ..
-func (helper HTTPHelper) PostForm(endpoint string, params map[string]string, form map[string]string) (*http.Response, *models.MonzoError, error) {
-	requestURL, err := url.Parse(fmt.Sprintf(helper.BaseURL, endpoint))
+const (
+	authorizationHeaderContent = "Bearer %s"
+)
+
+// Post makes a POST request
+func (helper HTTPHelper) Post(endpoint string, params map[string]string, form map[string]string, token *models.Token) (*http.Response, *models.MonzoError, error) {
+	return helper.makeRequest("POST", endpoint, params, form, token)
+}
+
+// Get ..
+func (helper HTTPHelper) Get(endpoint string, params map[string]string, token *models.Token) (*http.Response, *models.MonzoError, error) {
+	return helper.makeRequest("GET", endpoint, params, nil, token)
+}
+
+// makeRequest ..
+func (helper HTTPHelper) makeRequest(method, endpoint string, params map[string]string, form map[string]string, token *models.Token) (*http.Response, *models.MonzoError, error) {
+	reqURL, err := formatURL(helper.BaseURL, endpoint, params)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	for k, v := range params {
-		requestURL.Query().Add(k, v)
-	}
-
-	formContent := url.Values{}
-	for k, v := range form {
-		formContent.Add(k, v)
-	}
-
-	req, err := http.NewRequest("POST", requestURL.String(), bytes.NewBufferString(formContent.Encode()))
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req, err := createRequest(method, reqURL, form)
 	if err != nil {
 		return nil, nil, err
+	}
+
+	if token != nil {
+		req.Header.Add("Authorization", fmt.Sprintf(authorizationHeaderContent, token.AccessToken))
 	}
 
 	resp, err := helper.Client.Do(req)
@@ -54,6 +62,7 @@ func (helper HTTPHelper) PostForm(endpoint string, params map[string]string, for
 	}
 
 	if strings.Contains(resp.Header.Get("Content-Type"), "application/json") {
+		defer resp.Body.Close()
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			return nil, nil, err
@@ -75,17 +84,55 @@ func NewHTTPHelper() *HTTPHelper {
 	}
 }
 
-// UnmarshalJSON takes in a Http Response Body and Unmarshals it into it's JSON interface
+// UnmarshalJSON takes in a HTTP response body and decodes it into a JSON interface
 func UnmarshalJSON(body io.ReadCloser, v interface{}) error {
-	data, err := ioutil.ReadAll(body)
-	if err != nil {
-		return err
-	}
-
-	err = json.Unmarshal(data, &v)
+	defer body.Close()
+	err := json.NewDecoder(body).Decode(&v)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+// formatURL creates a URL with base, endpoint and optional params
+func formatURL(baseURL, endpoint string, params map[string]string) (*url.URL, error) {
+	reqURL, err := url.Parse(fmt.Sprintf(baseURL, endpoint))
+	if err != nil {
+		return nil, err
+	}
+
+	// Add Query Params if they exist
+	if len(params) > 0 {
+		q := reqURL.Query()
+		for k, v := range params {
+			q.Add(k, v)
+		}
+		reqURL.RawQuery = q.Encode()
+	}
+
+	return reqURL, nil
+}
+
+// createRequest creates a request with method, url, and optional form data
+func createRequest(method string, reqURL *url.URL, form map[string]string) (*http.Request, error) {
+	if len(form) <= 0 {
+		return http.NewRequest(method, reqURL.String(), nil)
+	}
+
+	// Create form content
+	formContent := url.Values{}
+	for k, v := range form {
+		formContent.Add(k, v)
+	}
+
+	// Create Request
+	req, err := http.NewRequest(method, reqURL.String(), bytes.NewBufferString(formContent.Encode()))
+	if err != nil {
+		return nil, err
+	}
+
+	// Add form content type
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	return req, nil
 }
